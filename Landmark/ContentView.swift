61ea5880fa3c7ba8877c6f0e22bd4fb91f866d13
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2020-01-29 21:07:30 +0800

    Initial Commit

---
+++ Landmark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmark
+//
+//  Created by boychenney on 2020/1/29.
+//  Copyright © 2020 boychenney. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
